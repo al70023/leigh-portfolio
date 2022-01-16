e734216a19d566cd1f8e786ce6556e54b0b76885
al70023 <87347668+al70023@users.noreply.github.com>
2022-01-15 22:31:56 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 500) {
+        document.getElementById('navigation').classList.add('fixed-top');
+        // add padding top to show content behind navbar
+        navbar_height = document.querySelector('.navbar').offsetHeight;
+        document.body.style.paddingTop = navbar_height + 'px';
+      } else {
+        document.getElementById('navigation').classList.remove('fixed-top');
+         // remove padding top from body
+        document.body.style.paddingTop = '0';
+      }
+  });
+});
